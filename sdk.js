37eefe187dd4e34ca74bbf897ba7c55b8b92fce5
Jaimi Sheta <67229735+jaimisheta@users.noreply.github.com>
2022-11-23 12:03:59 -0400

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -1,4 +1,8 @@
 (function (window, t) {
     var wcU = decodeURIComponent(window.location.href)
+    var wcD = Date.now()
+    var wcS = document.getElementsByTagName('HTML')[0].outerHTML.length
     console.log('url', wcU)
+    console.log('date', wcD)
+    console.log('size', wcS)
 })(this)
